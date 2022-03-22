5b618d295754f884fca31187b4648a83a3e6a985
wujie <wujie@aobi.com>
2022-03-22 16:43:54 +0800

    摇杆处理

--- Unity/Assets/HotfixView/Demo/UI/UIShootGame/UIShootGameComponentSystem.cs
+++ Unity/Assets/HotfixView/Demo/UI/UIShootGame/UIShootGameComponentSystem.cs
@@ -8,19 +8,97 @@ namespace ET
         public override void Awake(UIShootGameComponent self)
         {
             ReferenceCollector rc = self.GetParent<UI>().GameObject.GetComponent<ReferenceCollector>();
-			
-            self.testBtn = rc.Get<GameObject>("TestBtn");
-            self.testBtn.GetComponent<Button>().onClick.AddListener(self.Test);
-            self.testText = rc.Get<GameObject>("TestText").GetComponent<Text>();
+
+            self.MoveJointedArm = rc.Get<GameObject>("MoveJointedArm").GetComponent<JointedArm>();
+            self.RotateJointedArm = rc.Get<GameObject>("RotateJointedArm").GetComponent<JointedArm>();
+            self.FireBtn = rc.Get<GameObject>("FireButton");
+            self.BombBtn = rc.Get<GameObject>("BombButton");
+            self.JumpBtn = rc.Get<GameObject>("JumpButton");
+            self.BulletBtn = rc.Get<GameObject>("BulletButton");
+
+            self.MoveJointedArm.onDragCb = (direction) =>
+            {
+                self.MoveDragCallback(direction);
+            };
+
+            self.MoveJointedArm.onStopCb = () =>
+            {
+                self.StopMoveCallback();
+            };
+
+
+            self.RotateJointedArm.onDragCb = (direction) =>
+            {
+                self.RotateDragCallback(direction);
+            };
+
+            self.RotateJointedArm.onStopCb = () =>
+            {
+                self.StopRotateCallback();
+            };
+
+
+            // 开炮
+            EventTriggerListener.Get(self.FireBtn).onDown += (btn) =>
+            {
+                Debug.LogError("(self.FireBtn).onDown");
+
+                //EventDispatcher.instance.DispatchEvent(EventNameDef.FIRE, true);
+            };
+
+            EventTriggerListener.Get(self.FireBtn).onUp += (btn) =>
+            {
+                Debug.LogError("(self.FireBtn).onUp");
+
+                //EventDispatcher.instance.DispatchEvent(EventNameDef.FIRE, false);
+            };
+            // 丢手雷
+            EventTriggerListener.Get(self.BombBtn).onClick += (btn) =>
+            {
+                Debug.LogError("(self.BombBtn).onClick");
+
+                //EventDispatcher.instance.DispatchEvent(EventNameDef.BOMB);
+            };
+            // 跳跃
+            EventTriggerListener.Get(self.JumpBtn).onClick += (btn) =>
+            {
+                Debug.LogError("(self.JumpBtn).onClick");
+
+                //EventDispatcher.instance.DispatchEvent(EventNameDef.JUMP);
+            };
+            // 装子弹
+            EventTriggerListener.Get(self.BulletBtn).onClick += (btn) =>
+            {
+                Debug.LogError("(self.BulletBtn).onClick");
+
+                //EventDispatcher.instance.DispatchEvent(EventNameDef.BULLET);
+            };
         }
     }
     
     public static class UIShootGameComponentSystem
     {
-        public static void Test(this UIShootGameComponent self)
+        public static void MoveDragCallback(this UIShootGameComponent self, Vector3 direction)
         {
-            Debug.LogError("UIShootGameComponent.Test");
-            ShootGameHelper.ThrowBump(self.ZoneScene()).Coroutine();
+            Debug.LogError("MoveDragCallback");
+        }
+
+        public static void StopMoveCallback(this UIShootGameComponent self)
+        {
+            Debug.LogError("StopMoveCallback");
+
+        }
+
+        public static void RotateDragCallback(this UIShootGameComponent self, Vector3 direction)
+        {
+            Debug.LogError("RotateDragCallback");
+
+        }
+
+        public static void StopRotateCallback(this UIShootGameComponent self)
+        {
+            Debug.LogError("StopRotateCallback");
+
         }
     }
 }
--- Unity/Assets/ModelView/Demo/UI/UIShootGame/UIShootGameComponent.cs
+++ Unity/Assets/ModelView/Demo/UI/UIShootGame/UIShootGameComponent.cs
@@ -6,7 +6,29 @@ namespace ET
 {
 	public class UIShootGameComponent : Entity
 	{
-		public GameObject testBtn;
-		public Text testText;
-	}
+        /// <summary>
+        /// 移动摇杆
+        /// </summary>
+        public JointedArm MoveJointedArm;
+        /// <summary>
+        /// 旋转摇杆
+        /// </summary>
+        public JointedArm RotateJointedArm;
+        /// <summary>
+        /// 开枪按钮
+        /// </summary>
+        public GameObject FireBtn;
+        /// <summary>
+        /// 丢手雷按钮
+        /// </summary>
+        public GameObject BombBtn;
+        /// <summary>
+        /// 跳跃按钮
+        /// </summary>
+        public GameObject JumpBtn;
+        /// <summary>
+        /// 装子弹按钮
+        /// </summary>
+        public GameObject BulletBtn;
+    }
 }
